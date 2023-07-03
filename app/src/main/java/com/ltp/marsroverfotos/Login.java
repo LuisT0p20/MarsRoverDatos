7d9159f287d658294bda01ad2eab466b64c321c1
LuisT0p20 <luistoribiopalacios@gmail.com>
2023-07-03 17:21:49 -0500

    Login Register

--- app/src/main/java/com/ltp/marsroverfotos/Login.java
+++ app/src/main/java/com/ltp/marsroverfotos/Login.java
@@ -1,14 +1,87 @@
 package com.ltp.marsroverfotos;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
 
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    EditText edtEmail,edtPassword;
+    Button btnSignIn,btnSignUp;
+    AwesomeValidation awesomeValidation;
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        edtEmail = findViewById(R.id.edtEmailIn);
+        edtPassword = findViewById(R.id.edtPasswordIn);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        btnSignUp.setOnClickListener(this);
+        //Obtener instancia de firebase
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseAuth mAuth =FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user != null){
+            iraPrincipal();
+        }
+
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+        awesomeValidation.addValidation(this,R.id.edtEmailIn, Patterns.EMAIL_ADDRESS,R.string.validation_email);
+        awesomeValidation.addValidation(this,R.id.edtPasswordIn, ".{6,}",R.string.validation_password);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (awesomeValidation.validate()){
+                    String email = edtEmail.getText().toString();
+                    String password = edtPassword.getText().toString();
+
+                    firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                iraPrincipal();
+
+                            }else {
+                                String error = ((FirebaseAuthException) task.getException()).getErrorCode();
+                                Toast.makeText(Login.this, error, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void iraPrincipal() {
+        Intent intent = new Intent(this, Menu.class);
+        intent.putExtra("email",edtEmail.getText().toString());
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/ltp/marsroverfotos/Register.java
+++ app/src/main/java/com/ltp/marsroverfotos/Register.java
@@ -1,14 +1,62 @@
 package com.ltp.marsroverfotos;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Register extends AppCompatActivity {
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 
+public class Register extends AppCompatActivity implements View.OnClickListener {
+    EditText edtEmailRegister,edtPasswordRegister;
+    Button btnCreate;
+    FirebaseAuth firebaseAuth;
+    AwesomeValidation awesomeValidation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        edtEmailRegister = findViewById(R.id.edtEmailRegister);
+        edtPasswordRegister = findViewById(R.id.edtPassRegister);
+        btnCreate = findViewById(R.id.btnCreateAccount);
+        btnCreate.setOnClickListener(this);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+        awesomeValidation.addValidation(this,R.id.edtEmailRegister, Patterns.EMAIL_ADDRESS,R.string.validation_email);
+        awesomeValidation.addValidation(this,R.id.edtPassRegister, ".{6,}",R.string.validation_password);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String email =edtEmailRegister.getText().toString();
+        String password =edtPasswordRegister.getText().toString();
+        if (awesomeValidation.validate()){
+            firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(Register.this, "Usuario creado con exito", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }else{
+                        String error = ((FirebaseAuthException) task.getException()).getErrorCode();
+                        Toast.makeText(Register.this, error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }else{
+            Toast.makeText(this, "Datos incorrectos", Toast.LENGTH_SHORT).show();
+        }
     }
 }
